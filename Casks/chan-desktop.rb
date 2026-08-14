0d20d897e2abf79cbfdc15b6562343a38d8ea3ce
Alexandre Fiori <fiorix@gmail.com>
2026-08-14 18:32:42 +0000

    chan-desktop 0.90.0, chan 0.90.0

--- Casks/chan-desktop.rb
+++ Casks/chan-desktop.rb
@@ -3,8 +3,8 @@
 # to the fiorix/homebrew-chan tap at this relative path. The DMG asset name
 # must match web/packages/marketing/scripts/release-assets.mjs (desktopAssets).
 cask "chan-desktop" do
-  version "0.89.0"
-  sha256 "fddf44f5e894deb5ae541630597b54cbff248878fb70b7d659d6a36010ed7cff"
+  version "0.90.0"
+  sha256 "f7cc2c603f83b6b51970b414cce84a56d0884327b0ae42facf77f847fdc08c0e"
 
   url "https://github.com/fiorix/chan/releases/download/v#{version}/Chan_#{version}.dmg",
       verified: "github.com/fiorix/chan/"
